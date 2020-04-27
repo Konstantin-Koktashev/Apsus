ca5ff295d4a651817e9a9ac418042aef7e2062b7
Konstantin-Koktashev <lexxtalionis33@gmail.com>
2020-04-27 19:59:30 +0200

    Before push

--- EmailApp/EmailComponents/EmailList.jsx
+++ EmailApp/EmailComponents/EmailList.jsx
@@ -1,6 +1,7 @@
 import { EmailPreview } from "./EmailPreview.jsx";
 
 export function EmailList(props){
+    console.log("EmailList -> props", props)
     return(
         <div className="email-list">
         { props.emails.map(email => <EmailPreview key={ email.id } email={ email } />) }
